423e08fc8e8c3229ee43e0cd299cfdf47142ac02
adarsh <adarsh0ranjan@gmail.com>
2022-07-27 09:11:04 +0530

    Initial Commit

---
+++ project-8-astronut/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  project-8-astronut
+//
+//  Created by Roro Solutions on 27/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ project-8-astronut/project_8_astronutApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_8_astronutApp.swift
+//  project-8-astronut
+//
+//  Created by Roro Solutions on 27/07/22.
+//
+
+import SwiftUI
+
+@main
+struct project_8_astronutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
